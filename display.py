2272d4ab906c50207def816cf6a369ad88a8dbc0
GG@GG.GG <GLG@GG.GG>
2021-10-31 22:10:10 -0700

    Added path

--- display.py
+++ display.py
@@ -12,6 +12,10 @@ import traceback
 
 class Display:
     picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+    libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+    if os.path.exists(libdir):
+        sys.path.append(libdir)
+
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
