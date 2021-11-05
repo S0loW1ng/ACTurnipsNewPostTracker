14d9bb5d132523110a47ef171235c0c74f529aa1
GG@GG.GG <GLG@GG.GG>
2021-11-05 16:19:08 -0700

    Lib change

--- display.py
+++ display.py
@@ -1,18 +1,17 @@
 import sys
 import os
-from waveshare_epd import epd_2in13_V3
+from waveshare_epd import epd2in13d
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 
-epd = epd_2in13_V3.EPD()
+epd = epd2in13d.EPD()
 epd.init(0)
 epd.Clear()
 Himage = Image.open("lolz.jpg")  # 0xFF: clear the f$
 epd.display(epd.getbuffer(Himage))
 epd.sleep()
 
-epd_2in13_V3.epdconfig.module_exit()
 
 
 
