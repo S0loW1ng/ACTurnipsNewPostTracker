bbc9ee2ff2b02131c526522e7a59ea44a208c737
GG@GG.GG <GLG@GG.GG>
2021-11-05 16:04:13 -0700

    Added new screen

--- display.py
+++ display.py
@@ -1,18 +1,18 @@
 import sys
 import os
-from waveshare_epd import epd_2in13bc_test
+from waveshare_epd import epd_2in13_V3
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 
-epd = epd_2in13bc_test.EPD()
+epd = epd_2in13_V3.EPD()
 epd.init(0)
 epd.Clear()
 Himage = Image.open("lolz.jpg")  # 0xFF: clear the f$
 epd.display(epd.getbuffer(Himage))
 epd.sleep()
 
-epd_2in13bc_test.epdconfig.module_exit()
+epd_2in13_V3.epdconfig.module_exit()
 
 
 
