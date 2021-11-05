db753c08413264ffd2e5e3e73714db25aebc392b
GG@GG.GG <GLG@GG.GG>
2021-11-05 16:39:28 -0700

    Extension change

--- display.py
+++ display.py
@@ -1,13 +1,12 @@
 import sys
 import os
-from waveshare_epd import epd2in13d
+from waveshare_epd import epd2in13b_V3
 import time
 from PIL import Image,ImageDraw,ImageFont
-import traceback
 
-epd = epd2in13d.EPD()
+epd = epd2in13b_V3.EPD()
 epd.init()
-epd.Clear(0xFF)
+epd.Clear()
 Himage = Image.open("lolz.bmp")  # 0xFF: clear the f$
 epd.display(epd.getbuffer(Himage))
 epd.sleep()
