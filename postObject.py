935f69e51fad8844653267c887b5bb4c4f79e12e
GG@GG.GG <GLG@GG.GG>
2021-11-07 13:50:01 -0800

    Changed Date time format

--- postObject.py
+++ postObject.py
@@ -16,13 +16,13 @@ class post:
         print(self.url)
 
     def toString(self):
-        return str(str(self.title )+ " " + str(self.date))
+        return str(self.title) + " " + str(self.date)
 
-    def deEmojify(self,text):
-        regrex_pattern = re.compile(pattern = "["
-            u"\U0001F600-\U0001F64F"  # emoticons
-            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-            u"\U0001F680-\U0001F6FF"  # transport & map symbols
-            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags = re.UNICODE)
-        return regrex_pattern.sub(r'',text)
+    def deEmojify(self, text):
+        regrex_pattern = re.compile(pattern="["
+                                            u"\U0001F600-\U0001F64F"  # emoticons
+                                            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                            "]+", flags=re.UNICODE)
+        return regrex_pattern.sub(r'', text)
