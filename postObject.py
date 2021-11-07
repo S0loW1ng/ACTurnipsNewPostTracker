9a886417ebeecbd2fb5312ee59202561592ec1a0
GG@GG.GG <GLG@GG.GG>
2021-11-07 13:21:37 -0800

    Removed emojis

--- postObject.py
+++ postObject.py
@@ -1,5 +1,5 @@
 import datetime
-
+import re
 
 
 class post:
@@ -15,6 +15,12 @@ class post:
         print(self.url)
 
     def toString(self):
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   "]+", flags=re.UNICODE)
         title = self.title
-        title.encode('utf-8', 'ignore')
-        return str(str(title)+ " " + str(self.date))
+        emoji_pattern.sub(r'', title)
+        return str(str(title) + " " + str(self.date))
