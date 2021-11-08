62443fda136cac12c38d344fb83248c1c1abd380
GG@GG.GG <GLG@GG.GG>
2021-11-07 21:32:00 -0800

    changed relative path

--- display.py
+++ display.py
@@ -9,8 +9,8 @@ epd = epd2in13b_V3.EPD()
 epd.init()
 epd.Clear()
 time.sleep(1)
-Himage = Image.open("./lolz.bmp")  # 0xFF: clear the f$
-HRYimage = Image.open("./Logo.png")
+Himage = Image.open("/home/pi/ACTurnipsNewPostTracker/lolz.bmp")  # 0xFF: clear the f$
+HRYimage = Image.open("/home/pi/ACTurnipsNewPostTracker/Logo.png")
 epd.display(epd.getbuffer(Himage),epd.getbuffer(HRYimage))
 time.sleep(2)
 epd.sleep()
