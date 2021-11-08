8ae8747105dbc021b331e5bae7dc19ed32cf7268
GG@GG.GG <GLG@GG.GG>
2021-11-07 19:42:26 -0800

    Added Logo

--- display.py
+++ display.py
@@ -6,11 +6,11 @@ from PIL import Image,ImageDraw,ImageFont
 
 
 epd = epd2in13b_V3.EPD()
-HRYimage = Image.new('1', (epd.height, epd.width), 0xFF)
 epd.init()
 epd.Clear()
 time.sleep(1)
 Himage = Image.open("./lolz.bmp")  # 0xFF: clear the f$
+HRYimage = Image.open("./Logo.png")
 epd.display(epd.getbuffer(Himage),epd.getbuffer(HRYimage))
 time.sleep(2)
 epd.sleep()
